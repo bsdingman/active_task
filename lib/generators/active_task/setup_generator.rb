2c1984a907265720eb158f5764ca67a1f6340048
Bryan Dingman <bsdingman@gmail.com>
2019-09-01 21:18:57 -0400

    Insert requirements in development

--- lib/generators/active_task/setup_generator.rb
+++ lib/generators/active_task/setup_generator.rb
@@ -8,6 +8,13 @@ module ActiveTask
       desc "This generator creates a ActiveTask initializer"
       def create
         template "initializer.erb", "config/initializers/active_task.rb"
+
+        inject_into_file 'config/environments/development.rb', before: /^end/ do <<-'RUBY'
+
+  # Allow ActiveTask to check for pending tasks
+  config.middleware.use(ActiveTask::Middleware)
+      RUBY
+      end
       end
     end
   end
